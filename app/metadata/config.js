92ced7ad3ea6f727016d8b9c12211ffd8bafcfe8
Adrian Barber <abarber@standard.net>
2016-05-15 18:07:18 -0600

    Migrated settings for Gigya, Google and Mather from global config.js

--- app/metadata/config.js
+++ app/metadata/config.js
@@ -1,6 +1,11 @@
 module.exports = {
   baseUrl: '/',
   baseCanonicalUrl: 'https://github.com/seiops/cranberry/',
-  // Google Analytics Tracking ID
-  googleAnalyticsTrackingId: 'UA-XXXX-Y'
+    // Gigya
+  gigya: {
+    apiKey: '3_6UHHWrJ4LmAOWWdgqP0UWqk-2InoMn5NH8Lo1aOfcmFl6zAS4u_-IxvC3mbGAxch'
+  },
+  google: {
+    analytics: 'UA-746696-1'
+  }
 };
