8486202954e2ed0c9a811e56633ab66f8ae9e9cc
Michael Johnstone <mjohnstone@standard.net>
2016-05-20 12:35:07 -0600

    Removed all specific ad configs from config.js.

--- app/metadata/config.js
+++ app/metadata/config.js
@@ -11,21 +11,6 @@ module.exports = {
   dfpAdStructure: {
     adGroupID: 30103046,
     adGrouping: 'Ogden_Publisher',
-    adSubGrouping: 'Standard_Examiner',
-    top_of_stream: {
-      adSize: [[300,250],[300,600]]
-    },
-    middle_of_stream: {
-      adSize: [300,250]
-    },
-    bottom_of_stream: {
-      adSize: [300,250]
-    },
-    pencil_pushdown: {
-      adSize: [970,30]
-    },
-    leaderboard_bottom: {
-      adSize: [728, 90]
-    }
+    adSubGrouping: 'Standard_Examiner'
   }
 };
