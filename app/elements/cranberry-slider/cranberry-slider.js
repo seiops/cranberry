dc2f7ea6e61d7a708849ae40b2bf0fe94f705a5e
Michael Johnstone <mjohnstone@standard.net>
2016-06-20 10:44:17 -0600

    DEV CHECKPOINT Added awesome slider and its functionality to the shortcode gallery.

--- app/elements/cranberry-slider/cranberry-slider.js
+++ app/elements/cranberry-slider/cranberry-slider.js
@@ -3,7 +3,9 @@ class cranberrySlider {
     this.is = 'cranberry-slider';
     this.properties = {
       featured: {
-        type: Object
+        type: Object,
+        notify: true,
+        observer: 'featuredChanged'
       },
       links: {
         type: Array
@@ -11,5 +13,21 @@ class cranberrySlider {
     };
   }
 
+  featuredChanged() {
+    let myElement = this;
+    let awesomeSlider = this.$.slider;
+
+    this.featured.mediaAssets.images.forEach(function(value, index) {
+      if (index === 0) {
+        myElement.$.slider.removeChild(myElement.$.placehold);
+      }
+      let item = document.createElement('item');
+      item.setAttribute('source', 'http://www.standard.net' + value.url);
+      Polymer.dom(awesomeSlider).appendChild(item);
+    });
+
+    awesomeSlider.createdCallback();
+    awesomeSlider.setAttribute('bullets', 'true');
+  }
 }
 Polymer(cranberrySlider);
