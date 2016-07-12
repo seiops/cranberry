f38cd1f49afe19df3816eb6819148b8db8574f1b
Michael Johnstone <mjohnstone@standard.net>
2016-07-11 18:22:54 -0600

    Removed commented out code that was no longer in use

--- app/elements/cranberry-slider-wrapper/cranberry-slider-wrapper.js
+++ app/elements/cranberry-slider-wrapper/cranberry-slider-wrapper.js
@@ -29,47 +29,5 @@ class cranberrySliderWrapper {
       }
     };
   }
-
-  // attached() {
-  //
-  //   let awesomeSlider = this.$.slider;
-  //   let myElement = this;
-  //
-  //   this.async(function() {
-  //     let items = awesomeSlider.content.getDistributedNodes();
-  //     let oldNext = awesomeSlider.next;
-  //
-  //     myElement.featured.mediaAssets.images.forEach(function(value, index) {
-  //       if (index === 0) {
-  //         myElement.$.slider.removeChild(myElement.$.placehold);
-  //       }
-  //       let item = document.createElement('item');
-  //       item.setAttribute('source', 'http://www.standard.net' + value.url.toLowerCase());
-  //       Polymer.dom(awesomeSlider).appendChild(item);
-  //     });
-  //
-  //     awesomeSlider.next = function() {
-  //       // Call the next function as normal
-  //       oldNext.apply(this, arguments);
-  //       // Add new caption to caption area
-  //       let text = myElement.featured.mediaAssets.images[awesomeSlider.index + 1].caption;
-  //       myElement.$.caption.textContent = text;
-  //     }
-  //
-  //     // Rebuild awesome-slider element with new items
-  //     awesomeSlider.createdCallback();
-  //
-  //     // Remove the previous and next buttons from the info portion of awesome slider
-  //     let buttons = awesomeSlider.shadow.querySelector('.info').querySelectorAll('button');
-  //     buttons.forEach(function(value) {
-  //       value.style.display = 'none';
-  //     });
-  //
-  //     // Add a float style to the index/count span
-  //     let counterEl = awesomeSlider.shadow.querySelector('.info').querySelector('span');
-  //     counterEl.style.float = 'right';
-  //     counterEl.style.padding = '0 5px 0 0';
-  //   });
-  // }
 }
 Polymer(cranberrySliderWrapper);
