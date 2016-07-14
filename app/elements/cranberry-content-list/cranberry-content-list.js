0da053202a18491c8ba547e8de73453f0fe2894d
fawkstrot <abarber@standard.net>
2016-07-14 13:28:29 -0600

    Reworking logic for section observer to work with new cranberry-section element.

--- app/elements/cranberry-content-list/cranberry-content-list.js
+++ app/elements/cranberry-content-list/cranberry-content-list.js
@@ -10,109 +10,51 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 
 /* Gigya Socialize JS library integration */
 
-class cranberryContentList {
-
-  handleResponse (data) {
-    var restResponse = JSON.parse(data.detail.Result);
-
-    this.items = restResponse;
-
-    // var responseMessage = '';
-
-    // if (typeof restResponse !== undefined && restResponse.errorCode === 0) {
-
-    //   var params = {
-    //       provider:restResponse.loginProvider,
-    //       callback: this._onlogin(data),
-    //       UID: restResponse.UID,
-    //       UIDSignature: restResponse.UIDSignature,
-    //       signatureTimestamp: restResponse.signatureTimestamp
-    //   };
-
-    //   restResponse.callback = this._onlogin(data);
-
-
-    //   gigya.socialize.getUserInfo(params);
-
-    //   responseMessage = 'User: ' + restResponse.profile.nickname + '<br />UID: ' + restResponse.UID + '<br />Signature: ' + restResponse.UIDSignature + '<br />Provider: ' + restResponse.loginProvider;
-    // } else {
-    //   responseMessage = restResponse.errorDetails;
-    // }
-
-
-    // form.querySelector('.output').innerHTML = responseMessage;
-  }
+class CranberryContentList {
 
   beforeRegister() {
     this.is = 'cranberry-content-list';
     this.properties = {
-      rest: {
-        type: String,
-        value: "http://sedev.libercus.net/rest.json",
-        notify: true
+      count: {
+        type: Number
+      },
+      items: {
+        type: Object,
+        value: []
       },
       params: {
         type: Object,
         value: [],
-        observer: '_changeParams',
-        notify: true
+        observer: '_changeParams'
       },
-      items: {
-        type: Object,
-        value: [],
-        notify: true
-      },
-      type: {
+      rest: {
         type: String,
-        value: "default",
-        observer: '_changeType'
+        value: 'http://sedev.libercus.net/rest.json'
       },
       sections: {
         type: String,
-        value: "News",
         observer: '_changeSections'
       },
-      count: {
-        type: Number,
-        value: 10,
-        observer: '_changeCount'
-      },
       start: {
-        type: Number,
-        value: 1,
-        observer: '_changeStart'
+        type: Number
+      },
+      type: {
+        type: String
       }
     };
   }
 
   // Public methods.
-  attached() {
-    app.logger('\<content-list\> attached');
-
-    this._updateParams();
+  attached () {
+    app.logger('\<cranberry-content-list\> attached');
   }
 
-  ready() {
-    app.logger('\<content-list\> ready');
+  ready () {
+    app.logger('\<cranberry-content-list\> ready');
   }
-
 
   // Private methods
-  _checkItem(item,index) {
-    let modulus = index % 2;
-
-    if (index > 0 && modulus === 0) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  _changeCount() {
-
-  }
-
-  _changeParams() {
+  _changeParams () {
     let params = this.get('params');
 
     if (params.length !== 0 && params.desiredCount) {
@@ -124,19 +66,30 @@ class cranberryContentList {
     }
   }
 
-  _changeStart() {
+  _changeSections (section) {
+    app.logger ('<\cranberry-content-list\> section changed -\> ' + section);
 
+    this._updateParams();
   }
 
-  _changeSections() {
 
+  _checkItem (item, index) {
+    let modulus = index % 2;
+
+    if (index > 0 && modulus === 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
-  _changeType() {
+  _handleResponse (data) {
+    let restResponse = JSON.parse(data.detail.Result);
 
+    this.set('items', restResponse);
   }
 
-  _hasImage(image) {
+  _hasImage (image) {
     if(typeof image !== 'undefined' && image.length > 0) {
       return true;
     } else {
@@ -144,7 +97,7 @@ class cranberryContentList {
     }
   }
 
-  _hasPreview(preview) {
+  _hasPreview (preview) {
     if(typeof preview !== 'undefined' && preview.length > 0) {
       return true;
     } else {
@@ -152,7 +105,7 @@ class cranberryContentList {
     }
   }
 
-  _isStory(item) {
+  _isStory (item) {
     if(item === 'Story') {
       return true;
     } else {
@@ -160,7 +113,7 @@ class cranberryContentList {
     }
   }
 
-  _trimText(text) {
+  _trimText (text) {
     let trunc = text;
 
     if (trunc.length > 125) {
@@ -172,7 +125,11 @@ class cranberryContentList {
     return trunc;
   }
 
-  _updateParams() {
+  _updateParams () {
+    this.$.request.abortRequest();
+
+    this.set('items',[]);
+
     let jsonp = {};
 
     jsonp.request = "content-list";
@@ -185,4 +142,4 @@ class cranberryContentList {
 
 }
 
-Polymer(cranberryContentList);
+Polymer(CranberryContentList);
