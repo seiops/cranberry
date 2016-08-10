253b19b10064c82f1096d2c9a8af7c114f961fac
fawkstrot <abarber@standard.net>
2016-08-10 13:25:25 -0600

    Isolated Gigya elements and decoupled functionality.

--- app/elements/gigya-socialize/gigya-socialize.js
+++ app/elements/gigya-socialize/gigya-socialize.js
@@ -22,6 +22,10 @@ class GigyaSocialize {
       userSelected: {
         type: Number,
         value: 0
+      },
+      guestSelected: {
+        type: Number,
+        value: 0
       }
     };
   }
@@ -30,27 +34,7 @@ class GigyaSocialize {
     app.logger('\<gigya-socialize\> attached');
 
     this.async(function() {
-      this._addListeners();
       this._checkGigya();
-    });
-  }
-
-  _addListeners() {
-    let request = Polymer.dom(this.root).querySelector('#request');
-    let loginForm = Polymer.dom(this.root).querySelector('#loginForm');
-
-    loginForm.addEventListener('iron-form-submit', function(event) {
-      let params = {};
-
-      params.apiKey = '3_6UHHWrJ4LmAOWWdgqP0UWqk-2InoMn5NH8Lo1aOfcmFl6zAS4u_-IxvC3mbGAxch';
-      params.format = 'jsonp';
-      params.loginID = loginForm.loginID.value;
-      params.password = loginForm.password.value;
-
-      request.params = params;
-      request.url = 'https://accounts.us1.gigya.com/accounts.login';
-
-      request.generateRequest();
     });
   }
 
@@ -115,26 +99,6 @@ class GigyaSocialize {
     this.set('userSelected', 3);
   }
 
-  _handleLogin(event) {
-    app.logger('\<gigya-socialize\> handle login');
-
-    let form = Polymer.dom(event).localTarget.parentElement;
-
-    form.submit();
-  }
-
-  _handleLoginSocial(event) {
-    let provider = Polymer.dom(event).localTarget.getAttribute('provider');
-
-    let params = {
-        provider: provider,
-        callback: this._loadUser,
-        context: this
-    };
-
-    gigya.accounts.socialLogin(params);
-  }
-
   _handleLogout() {
     app.logger('\<gigya-socialize\> handle logout');
 
@@ -144,24 +108,6 @@ class GigyaSocialize {
     };
 
     gigya.accounts.logout(params);
-  }
-
-  _handleReset(event) {
-    let form = Polymer.dom(event).localTarget.parentElement;
-
-    form.reset();
-  }
-
-  _handleResponse(response) {
-    let detail = response.detail;
-
-    if (typeof detail !== undefined && detail.errorCode === 0) {
-      this._setUserCookie(detail.sessionInfo.cookieName, detail.sessionInfo.cookieValue, 365);
-
-      this.checkUser();
-    } else if (detail.errorCode !== 0 ) {
-      this._handleError(detail);
-    }
   }
 
   _loadAccount(account) {
@@ -215,16 +161,6 @@ class GigyaSocialize {
     el.set('user', {});
 
     gigya.socialize.refreshUI();
-  }
-
-  _setUserCookie(cname, cvalue, exdays) {
-    let d = new Date();
-
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-
-    let expires = "expires=" + d.toUTCString();
-
-    document.cookie = cname + "=" + cvalue + "; " + expires + "; path=/";
   }
 }
 
