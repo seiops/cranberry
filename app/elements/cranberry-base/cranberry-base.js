99217d8ae9bd5decc4fb454766df611b03f2073b
Michael Johnstone <mjohnstone@standard.net>
2016-08-11 18:04:53 -0600

    Added timeout function for hidden input.

--- app/elements/cranberry-base/cranberry-base.js
+++ app/elements/cranberry-base/cranberry-base.js
@@ -238,14 +238,17 @@ class CranberryBase {
 
     _focusSearch() {
       let drawer = this.$.drawer;
+      let drawerBar = this.$.drawerSearch;
 
       if (!drawer.opened) {
         // If the drawer is closed open it
         this.$.drawer.open();
+        // Timeout function to ensure the input is not hidden anymore
+        setTimeout(function () {drawerBar.$.input.focus()}, 250);
+      } else {
+        drawerBar.scaleElement();
+        drawerBar.$.input.focus()
       }
-      let drawerBar = this.$.drawerSearch;
-      drawerBar.scaleElement();
-      drawerBar.$.input.focus();
     }
 }
 // Change accent color
