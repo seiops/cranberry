7c4610f8bc9a126f1d2103d53ecd000aff27f2ed
Michael Johnstone <mjohnstone@standard.net>
2016-08-18 08:32:02 -0600

    Added sandusky-general and standard-general to keep history of these general configs. General.js will house the current build general config file.

---
+++ app/metadata/standard-general.js
@@ -0,0 +1,13 @@
+module.exports = {
+  appName: 'Cranberry',
+  appShortName: 'CRAN',
+  title: 'Stadard Examiner',
+  description: 'SNG Web Core',
+  charset: 'utf-8',
+  lang: 'en',
+  // Add to homescreen for Chrome on Android
+  applicationName: 'CRAN',
+  // Add to homescreen for Safari on iOS
+  appleMobileWebAppTitle: 'CRAN',
+  siteName: 'Standard-Examiner'
+};
