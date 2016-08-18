65967a6870bf3d16abd357d6ee084646babf689c
Michael Johnstone <mjohnstone@standard.net>
2016-08-18 17:48:43 -0600

    DEV CHECKPOINT

--- app/elements/cranberry-calendar/cranberry-calendar.js
+++ app/elements/cranberry-calendar/cranberry-calendar.js
@@ -12,14 +12,12 @@ class cranberryCalendar {
   _onRouteChanged(newValue) {
     let calendarContainer = this.$.calendarContainer;
     if (typeof newValue.path !== 'undefined' && newValue.path === '/calendar') {
-      let calendar = document.createElement('spingo-calendar');
-      calendar.id = 'spingoCalendar';
-      calendarContainer.appendChild(calendar);
-    } else {
-      let spingo = this.$$('#spingoCalendar');
-      if (calendarContainer.firstChild) {
-        calendarContainer.removeChild(spingo);
+      if (!calendarContainer.firstChild) {
+        let calendar = document.createElement('spingo-calendar');
+        calendar.id = 'spingoCalendar';
+        calendarContainer.appendChild(calendar);
       }
+
     }
   }
 }
