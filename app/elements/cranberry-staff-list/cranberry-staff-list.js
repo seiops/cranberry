074a580f7d1a9e3ded117cb50ebec9331a8b8401
Michael Johnstone <mjohnstone@standard.net>
2016-08-17 18:22:02 -0600

    Updated contact page. Added contact route. Adjusted logic for when JSONP request is sent. Added correct variables for items.

--- app/elements/cranberry-staff-list/cranberry-staff-list.js
+++ app/elements/cranberry-staff-list/cranberry-staff-list.js
@@ -5,22 +5,90 @@ class cranberryStaffList {
       items: {
         type: Object,
         value: []
+      },
+      rest: {
+        type: String
+      },
+      params: {
+          type: Object,
+          value: [],
+          observer: '_changeParams'
+      },
+      response: {
+          type: Object,
+          observer: '_parseResponse'
+      },
+      route: {
+        type: Object,
+        observer: '_onRouteChanged'
+      },
+      baseUrl: {
+        type: String
       }
     };
   }
 
-  _computeStaffImage(imageObject) {
-    if (typeof imageObject.small !== 'undefined') {
-      return 'http://www.standard.net/' + imageObject.small;
-    } else {
-      return 'http://imgsrc.me/200x113/9c9c9c/000000/Image Unavailable?showDimensions=0&font=arial';
+  _onRouteChanged(newValue) {
+    this.async(function() {
+        app.logger('\<cranberry-staff-list\> route changed -\> ' + newValue.path);
+        if (typeof newValue !== 'undefined' && newValue.path === '/contact') {
+          this._updateParams();
+        }
+    });
+  }
+
+  _updateParams() {
+    let items = this.get('items');
+
+    if (items.length === 0) {
+      let currentRequest = this.get('request');
+
+      if (typeof currentRequest !== 'undefined' && currentRequest.loading === true) {
+          app.logger('<\cranberry-staff-list\> aborting previous request');
+          this.$.request.abortRequest(currentRequest);
+      }
+
+      let jsonp = {};
+
+      jsonp.request = 'congero';
+      jsonp.desiredContent = 'staff';
+      jsonp.desiredStaff = 1;
+      this.set('params', jsonp);
     }
   }
 
-  _handleResponse (data) {
-    let restResponse = JSON.parse(data.detail.Result);
+  _changeParams() {
+    let params = this.get('params');
 
-    this.set('items', restResponse);
+    if (params.length !== 0) {
+        this.$.request.setAttribute('url', this.get('rest'));
+        this.$.request.params = params;
+        this.$.request.generateRequest();
+    }
+  }
+
+  _handleLoad() {
+      app.logger('<\cranberry-staff-list\> load received');
+  }
+
+  _handleResponse() {
+      app.logger('<\cranberry-staff-list\> response received');
+  }
+
+  _parseResponse(response) {
+      var result = JSON.parse(response.Result);
+
+      this.set('items', result);
+  }
+
+  _computeStaffImage(imageObject) {
+    let baseUrl = this.get('baseUrl');
+
+    if (typeof imageObject.small !== 'undefined') {
+      return baseUrl + '/' + imageObject.small;
+    } else {
+      return 'http://imgsrc.me/200x113/9c9c9c/000000/Image Unavailable?showDimensions=0&font=arial';
+    }
   }
 
 }
