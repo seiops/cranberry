f72e59f5c20b602c17f8368442e0d1a665359a17
Michael Johnstone <mjohnstone@standard.net>
2016-08-26 12:19:33 -0600

    Added core setup for scorestream widget. Also updated GPT script.

---
+++ app/elements/scorestream-widget/scorestream-widget.js
@@ -0,0 +1,144 @@
+class scorestreamWidget {
+  beforeRegister() {
+    this.is = 'scorestream-widget';
+    this.properties = {
+      widgetId: {
+        type: Number,
+        observer: '_updateUrl'
+      },
+      widgetUrl: {
+        type: String
+      },
+      scoreStreamUrl: {
+        type: String,
+        value: 'https://scorestream.com/widgets/scoreboards/horz?userWidgetId='
+      },
+      isOff: {
+        type: Boolean,
+        value: true,
+        oberver: '_changeVisibility'
+      },
+      route: {
+        type: Object
+      },
+      section: {
+        type: String,
+        observer: '_onSectionChanged'
+      },
+      response: {
+        type: Object
+      },
+      rest: {
+          type: String
+      },
+      response: {
+          type: Object,
+          observer: '_parseResponse'
+      },
+      items: {
+        type: Object
+      },
+      masterSwitch: {
+        type: Boolean,
+        value: false
+      },
+      scoreboardSwitch: {
+        type: Boolean,
+        value: false
+      },
+      hidden: {
+        type: Boolean,
+        value: true
+      }
+    }
+    this.observers = ['_turnOnOff(masterSwitch, scoreboardSwitch)'];
+  }
+
+  _handleLoad() {
+      app.logger('<\scorestream-widget\> load received');
+  }
+
+  _handleResponse() {
+      app.logger('<\scorestream-widget\> response received');
+  }
+
+  _parseResponse(response) {
+    var result = JSON.parse(response.Result);
+
+    if (Object.keys(result).length > 0 && typeof result.scoreboards !== 'undefined') {
+      this.set('masterSwitch', result.masterSwitch);
+      this.set('scoreboardSwitch', result.scoreboards[0].scoreboardOn);
+      this.set('widgetId', result.scoreboards[0].horizontalId);
+    } else {
+      this.set('isOff', true);
+    }
+  }
+
+  _turnOnOff(masterSwitch, scoreboardSwitch) {
+    if(masterSwitch) {
+      this.set('isOff', true);
+    } else {
+      if (scoreboardSwitch) {
+        this.set('isOff', true);
+      } else {
+        this.set('isOff', false);
+      }
+    }
+  }
+
+  _changeVisibility(off) {
+    if (off) {
+      this.hidden = true;
+    } else {
+      this.hidden = false;
+    }
+  }
+
+  _onSectionChanged() {
+    let route = this.get('route');
+    this.async(function() {
+      let hidden = this.get('hidden');
+
+      if (!hidden) {
+        if (typeof route.section !== 'undefined') {
+          let tags = this.get('tags');
+          let section = route.section;
+          let path = route.path;
+
+          let request = this.$.request;
+          let params = {};
+
+          this.set('widgetId', '');
+          this.set('widgetUrl', '');
+
+          params.request = 'congero';
+          params.desiredContent = 'scorestream';
+          request.setAttribute('url', this.get('rest'));
+
+          console.info(route);
+          // If homepage
+          if (section === '') {
+            params.desiredTeam = 'Home Scoreboard';
+          } else {
+            params.desiredTeam = section.replace(/-/g, ' ');
+          }
+
+          request.params = params;
+          request.generateRequest();
+        }
+      }
+    });
+  }
+
+  _updateUrl(newValue) {
+    if (newValue !== '') {
+      let scoreStreamUrl = this.get('scoreStreamUrl');
+
+      this.set('widgetUrl', scoreStreamUrl + newValue);
+    }
+  }
+
+
+
+}
+Polymer(scorestreamWidget);
