dc59c3e0d2b6b6ea05eeafeaa5a0678874dfbd8f
Michael Johnstone <mjohnstone@standard.net>
2016-09-07 08:36:43 -0600

    Added social urls to config file.

--- app/metadata/sandusky-config.js
+++ app/metadata/sandusky-config.js
@@ -32,6 +32,12 @@ module.exports = {
     facebook: 'sanduskyregistr',
     facebookId: '259812604228072'
   },
+  socialUrls: {
+    facebook: 'https://www.facebook.com/sanduskyregister/',
+    twitter: 'https://twitter.com/sanduskyregistr',
+    instagram: 'https://www.instagram.com/sanduskyregister/',
+    linkedin: 'https://www.linkedin.com/company/sandusky-register'
+  },
   // Tout
   tout: {
       toutUid: '277e2b'
