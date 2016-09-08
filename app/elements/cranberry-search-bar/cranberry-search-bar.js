053fdd9c1bf1f61c77306bbd318620a32c67e9af
Michael Johnstone <mjohnstone@standard.net>
2016-09-08 09:08:01 -0600

    Fixed anomaly with search bar validation.

--- app/elements/cranberry-search-bar/cranberry-search-bar.js
+++ app/elements/cranberry-search-bar/cranberry-search-bar.js
@@ -17,12 +17,28 @@ class cranberrySearchBar {
     this.async(function() {
       let el = this;
       let form = Polymer.dom(this.root).querySelector('#searchForm');
+      let input = Polymer.dom(this.root).querySelector('#input');
+      let value = this.get('query');
 
       form.addEventListener('iron-form-submit', function() {
         app.logger('\<cranberry-search-bar\> search submit event');
 
         el._search();
       });
+
+      // Custom validation function on focus lost
+      // This will ensure that errors will not show if the input doesnt have a value in it.
+      input.addEventListener('blur', function() {
+        // Test against value as input.value could be a null Object at this point.
+        if (value !== '') {
+          if (!input.validate()) {
+            input.set('invalid', true);
+          } else {
+            input.set('invalid', false);
+          }
+        }
+      });
+
     });
   }
 
