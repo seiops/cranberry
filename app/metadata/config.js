c4df0f320a504a36169d9dbbfce47ab93cb12236
Michael Johnstone <mjohnstone@standard.net>
2016-09-20 11:59:50 -0600

    Added all logic for news tip to link to modal. Added styles to the modal.

--- app/metadata/config.js
+++ app/metadata/config.js
@@ -88,7 +88,11 @@ module.exports = {
       {
         logo: 'icons:lightbulb-outline',
         label: 'News Tip',
-        phone: '419-502-2060'
+        phone: '419-502-2060',
+        link: true,
+        linkUrl: 'http://srdev.libercus.net/tell-us'
+        // Commented out production url. This site is not ready yet.
+        // linkUrl: 'tellus.sanduskyregister.com'
       },
       {
         logo: 'icons:chrome-reader-mode',
