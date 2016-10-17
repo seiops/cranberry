91cdfbd2c2fd846883b29ccc86838bc2afa68f1f
fawkstrot <fawkstrot@gmail.com>
2016-10-17 12:14:28 -0600

    Initial commit of cranberry-google-analytics JS module.

---
+++ app/elements/cranberry-google-analytics/cranberry-google-analytics.js
@@ -0,0 +1,75 @@
+class CranberryGoogleAnalytics {
+  // element registration
+  beforeRegister() {
+    this.is = 'cranberry-google-analytics';
+    this.properties = {
+      trackingId: String,
+      userId: {
+          type: String,
+          notify: true
+      }
+    };
+    this.listeners = {
+      'track-event': 'trackEvent',
+      'track-page': 'trackPage',
+      'user-id-changed': 'userIDChanged'
+    };
+  }
+
+  // public methods
+
+  // attached to document
+  attached() {
+    app.logger('\<cranberry-google-analytics\> attached');
+
+    this._checkGoogle();
+  }
+
+  _checkGoogle() {
+    let el = this;
+
+    setTimeout(function() {
+      if (typeof ga !== 'undefined') {
+        el._setupGA();
+
+        return;
+      } else {
+        el._checkGoogle();
+      }
+    }, 50);
+  }
+
+  _setupGA() {
+    let id = this.get('trackingId');
+    app.logger('\<cranberry-google-analytics\> setting tracking for ' + id);
+
+    ga('create', id, 'auto');
+  }
+
+  trackEvent(e) {
+      //Add support for a non-interaction even that does not effect bounce rates - eg things that happen after a timeout
+      //ga('send', 'event', 'category', 'action', {'nonInteraction': 1});
+      ga('send', 'event', e.detail.category, e.detail.action, e.detail.label, e.detail.value);
+  }
+
+  trackPage(e) {
+      //Use set param, this way if we then send a subsequent event on the page it will be correctly associated with the same page
+
+      if (typeof e !== 'undefined' && typeof e.detail.path !== 'undefined') {
+          ga('set', 'page', e.detail.path);
+      }
+
+      if(typeof e.detail.data !== 'undefined') {
+        app.logger('\<cranberry-google-analytics\> pageview sent with data');
+        ga('send', 'pageview', e.detail.data);
+      } else {
+        app.logger('\<cranberry-google-analytics\> pageview sent');
+        ga('send', 'pageview');
+      }
+  }
+
+  userIDChanged(e) {
+      ga('set', '&uid', this.userId);
+  }
+}
+Polymer(CranberryGoogleAnalytics);
