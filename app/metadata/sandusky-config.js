fe186750ec0051072c45ddb20ee35be53601f856
Michael Johnstone <mjohnstone@standard.net>
2016-10-27 14:39:44 -0600

    Adjusted link strucutre. Added button text variable. Adjusted logic within template. Tested

--- app/metadata/sandusky-config.js
+++ app/metadata/sandusky-config.js
@@ -141,13 +141,15 @@ module.exports = {
       description: 'The ePaper is designd for digital users who prefer the experience of the printed edition. This archive is updated daily with the most recent edition.',
       dates: '2014-09-01 - Present',
       link: '/ePaper',
+      buttonText: 'View ePaper',
       image: '../images/archive/sandusky/ipad_epaper.png'
     },
     {
       label: 'ePaper Archive',
       description: 'Coming Soon - The ePaper Archive is a deeper repository of printed editions, formatted for those users who prefer the print experience on a digital device.',
       dates: '2004-08-11 - 2014-08-31',
-      link: '#',
+      link: 'http://sample.olivesoftware.com/Olive/APA/SanduskyRegisterA/Default.aspx#panel=home',
+      buttonText: 'View Archive',
       image: '../images/archive/sandusky/e-paper_archive_image.png'
     },
     {
@@ -155,13 +157,15 @@ module.exports = {
       description: 'Search the Sandusky Register site for the stories, photos, videos and people that you are looking for.',
       dates: '2012-0-01 - Present',
       link: '/search',
+      buttonText: 'Search Site',
       image: '../images/archive/sandusky/search_image.png'
     },
     {
       label: 'Text Archive',
       description: 'The Sandusky Register Text Archive is an additional search tool that allows you to do more advanced searching. This archive contains only story text, and no photos or videos.',
       dates: '2001-05-22 - Present',
-      link: '/news-archive',
+      link: '/archive',
+      buttonText: 'Coming Soon',
       image: '../images/archive/sandusky/text_archive_image.png'
     }
   ],
