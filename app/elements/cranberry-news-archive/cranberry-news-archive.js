35799f5720cb9560865492fdb49aaf81d2bf4834
Michael Johnstone <mjohnstone@standard.net>
2016-10-28 14:43:37 -0600

    Made the form and adjusted styles.

--- app/elements/cranberry-news-archive/cranberry-news-archive.js
+++ app/elements/cranberry-news-archive/cranberry-news-archive.js
@@ -1,10 +1,28 @@
 class cranberryNewsArchive {
   beforeRegister() {
     this.is = 'cranberry-news-archive';
+    this.properties = {
+      yearArray: Array,
+      quickSearch: {
+        type: Boolean,
+        value: true
+      }
+    }
   }
 
-  ready() {
+  attached() {
+    let date = new Date();
+    let yearArray = [];
 
+    for (let i = 2001; i <= date.getFullYear(); i++) {
+      yearArray.push(i);
+    }
+
+    this.set('yearArray', yearArray);
+  }
+
+  _handleSubmit(event) {
+    Polymer.dom(event).localTarget.parentElement.submit();
   }
 }
 Polymer(cranberryNewsArchive);
