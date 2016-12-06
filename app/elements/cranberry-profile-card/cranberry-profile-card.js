b5b0b48bca69ad0b7f2f6894f43c858316176817
Michael Johnstone <mjohnstone@standard.net>
2016-12-06 13:20:02 -0700

    ABRUPT CHECKPOINT

--- app/elements/cranberry-profile-card/cranberry-profile-card.js
+++ app/elements/cranberry-profile-card/cranberry-profile-card.js
@@ -3,7 +3,44 @@ class cranberryProfileCard {
     this.is = 'cranberry-profile-card';
     this.properties = {
       profile: {
-        type: Object
+        type: Object,
+        observer: '_profileChanged'
+      },
+      hideEmail: {
+        type: Boolean,
+        value: true
+      },
+      hideFacebook: {
+        type: Boolean,
+        value: true
+      },
+      hideLinkedin: {
+        type: Boolean,
+        value: true
+      },
+      hideTwitter: {
+        type: Boolean,
+        value: true
+      },
+    }
+  }
+
+  _profileChanged(newValue, oldValue) {
+    if (typeof newValue !== 'undefined' && typeof newValue.socialInfo !== 'undefined') {
+      if (typeof newValue.socialInfo.email !== 'undefined' && newValue.socialInfo.email !== '') {
+        this.set('hideEmail', false);
+      }
+
+      if (typeof newValue.socialInfo.facebook !== 'undefined' && newValue.socialInfo.facebook !== '') {
+        this.set('hideFacebook', false);
+      }
+
+      if (typeof newValue.socialInfo.linkedin !== 'undefined' && newValue.socialInfo.linkedin !== '') {
+        this.set('hideLinkedin', false);
+      }
+
+      if (typeof newValue.socialInfo.twitter !== 'undefined' && newValue.socialInfo.twitter !== '') {
+        this.set('hideTwitter', false);
       }
     }
   }
