06e36da8711efe1e699653c2e95cea9371a777f8
Michael Johnstone <mjohnstone@standard.net>
2016-12-21 12:42:39 -0700

    Adjusted logic for promoted stories to have postrelease start in the script.

--- app/elements/cranberry-nativo-story/cranberry-nativo-story.js
+++ app/elements/cranberry-nativo-story/cranberry-nativo-story.js
@@ -1,24 +1,51 @@
 class CranberryNativoStory {
     beforeRegister() {
-        this.is = 'cranberry-nativo-story';
+      this.is = 'cranberry-nativo-story';
+      this.properties = {
+        hidden: {
+          type: Boolean,
+          value: true,
+          reflectToAttribute: true,
+          observer: 'hiddenChanged'
+        }
+      }
     }
 
     attached() {
-        console.info('\<cranberry-nativo-story\> attached');
+      console.info('\<cranberry-nativo-story\> attached');
+    }
+
+    hiddenChanged(hidden, oldHidden) {
+      console.info('\<cranberry-nativo-story\> hiddenChanged');
+
+      this.async(() => {
+        if (typeof hidden !== 'undefined' && typeof PostRelease !== 'undefined' && typeof PostRelease.Start === 'function' && !hidden) {
+          console.info('\<cranberry-nativo-story\> firing PostRelease.Start');
+          PostRelease.Start();
+        } else if ((typeof PostRelease === 'undefined' || PostRelease.Start !== 'function') && !hidden) {
+          let PostReleaseDefined= new Promise(
+            function(resolve, reject) {
+              function timeoutFunction() {
+                console.info('\<cranberry-nativo-story\> PostRelease Not Defined');
+                setTimeout(function() {
+                  if (typeof PostRelease !== 'undefined' && typeof PostRelease.Start === 'function') {
+                    resolve(true);
+                    return;
+                  } else {
+                    timeoutFunction();
+                  }
+                }, 50);
+              }
+              timeoutFunction();
+            }
+          );
+
+          PostReleaseDefined.then(function(val) {
+            console.info('\<cranberry-nativo-story\> firing PostRelease.Start');
+            PostRelease.Start();
+          });
+        }
+      });
     }
 }
-// Public methods.
-// ready () {
-//   console.info('\<cranberry-story\> ready');
-// }
-// Private methods.
-// Called by observer when params object is changed.
-// Updates id value from route.
-// Calculates byline display.
-// Processes shortcodes and displays content.
-// Format JSON response and set story object.
-// Unknown functionality, unable to trace.
-// Scrolls to comment area.
-// Observer method for when the story id changes.
-// Update story id in request parameters.
 Polymer(CranberryNativoStory);
