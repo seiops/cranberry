72ba5e7ca56cd412283407a5ec659a6a030a8e90
Michael Johnstone <mjohnstone@standard.net>
2017-05-16 13:52:55 -0600

    Added new checking method that will catch the majority of browsers.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -52,19 +52,44 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   // Check browser version if web components are not supported
   if (!webComponentsSupported) {
     if (typeof navigator !== 'undefined' && navigator.userAgent !== 'undefined') {
-      let userAgent = navigator.userAgent;
-      if (userAgent.indexOf("Trident/7.0") > 0) {
-        console.log('I am IE 11');
-        showBlockingSplash();
-      } else if (userAgent.indexOf("Trident/6.0") > 0) {
-        console.log('I am IE 10');
-      } else if (userAgent.indexOf("Trident/5.0") > 0) {
-        console.log('I am IE 9');
-      } else {
-        console.log('I am not a triggered version of IE');
+      let browser = get_browser();
+      
+      switch(browser.name) {
+        case 'IE':
+          console.log('I am IE version: ', browser.version);
+          break;
+        
       }
+      // if (userAgent.indexOf("Trident/7.0") > 0) {
+      //   console.log('I am IE 11');
+      //   showBlockingSplash();
+      // } else if (userAgent.indexOf("Trident/6.0") > 0) {
+      //   console.log('I am IE 10');
+      // } else if (userAgent.indexOf("Trident/5.0") > 0) {
+      //   console.log('I am IE 9');
+      // } else {
+      //   console.log('I am not a triggered version of IE');
+      // }
     }
   }
+
+  function get_browser() {
+    var ua=navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || []; 
+    if(/trident/i.test(M[1])){
+        tem=/\brv[ :]+(\d+)/g.exec(ua) || []; 
+        return {name:'IE',version:(tem[1]||'')};
+        }   
+    if(M[1]==='Chrome'){
+        tem=ua.match(/\bOPR|Edge\/(\d+)/)
+        if(tem!=null)   {return {name:'Opera', version:tem[1]};}
+        }   
+    M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+    if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
+    return {
+      name: M[0],
+      version: M[1]
+    };
+ }
 
   function showBlockingSplash() {
     app.setAttribute('hidden', true);
