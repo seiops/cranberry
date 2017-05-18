f103af6c21848ea7defa8966e6688c36180febd0
Michael Johnstone <mjohnstone@standard.net>
2017-05-18 09:24:48 -0600

    Added logic for splash screen. Added HTML markup. Added triggers. Added CSS for splash screen.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -40,51 +40,46 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
     'import' in document.createElement('link') &&
     'content' in document.createElement('template'));
 
-  var fetchSupported = (typeof window.fetch === 'function' ? true : false);
+  let fetchSupported = (typeof window.fetch === 'function' ? true : false);
 
   if (!fetchSupported) {
-    var fetchPolyfill = document.createElement('script');
+    let fetchPolyfill = document.createElement('script');
     fetchPolyfill.src = 'bower_components/fetch/fetch.js';
 
     document.body.appendChild(fetchPolyfill);
   }
 
+  // Establish Browser Versioning Object
+  app.browser = get_browser();
+
   // Check browser version if web components are not supported
   if (!webComponentsSupported) {
-    if (typeof navigator !== 'undefined' && navigator.userAgent !== 'undefined') {
-      let browser = get_browser();
-      
-      switch(browser.name) {
-        case 'IE':
-          console.log('I am IE version: ', browser.version);
-          break;
-        
-      }
-      // if (userAgent.indexOf("Trident/7.0") > 0) {
-      //   console.log('I am IE 11');
-      //   showBlockingSplash();
-      // } else if (userAgent.indexOf("Trident/6.0") > 0) {
-      //   console.log('I am IE 10');
-      // } else if (userAgent.indexOf("Trident/5.0") > 0) {
-      //   console.log('I am IE 9');
-      // } else {
-      //   console.log('I am not a triggered version of IE');
-      // }
+    switch(app.browser.name) {
+      case 'IE':
+        showBlockingSplash();
+        break;
     }
   }
 
   function get_browser() {
-    var ua=navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || []; 
-    if(/trident/i.test(M[1])){
-        tem=/\brv[ :]+(\d+)/g.exec(ua) || []; 
-        return {name:'IE',version:(tem[1]||'')};
-        }   
-    if(M[1]==='Chrome'){
-        tem=ua.match(/\bOPR|Edge\/(\d+)/)
-        if(tem!=null)   {return {name:'Opera', version:tem[1]};}
-        }   
+    let ua = navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+
+    if (/trident/i.test(M[1])) {
+      tem=/\brv[ :]+(\d+)/g.exec(ua) || []; 
+      return {name:'IE',version:(tem[1]||'')};
+    }
+
+    if (M[1]==='Chrome') {
+      tem=ua.match(/\bOPR|Edge\/(\d+)/)
+      if(tem!=null)   {return {name:'Opera', version:tem[1]};}
+    }
+
     M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
-    if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
+
+    if((tem=ua.match(/version\/(\d+)/i))!=null) {
+      M.splice(1,1,tem[1]);
+    }
+    
     return {
       name: M[0],
       version: M[1]
