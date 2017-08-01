37584464a7fcaa1cbd4bc4c9624d27787f3496bd
Michael Johnstone <mjohnstone@standard.net>
2017-08-01 16:23:39 -0600

    DEV CHECKPOINT

--- app/elements/cxense-recommendation-widget/cxense-recommendation-widget.js
+++ app/elements/cxense-recommendation-widget/cxense-recommendation-widget.js
@@ -4,5 +4,26 @@ class cxenseRecommendationWidget {
     this.properties = {
     };
   }
+
+  attached() {
+    let cX = cX || {};
+    cX.callQueue = cX.callQueue || [];
+
+    console.dir(cX);
+    console.dir(cX.callQueue);
+    cX.callQueue.push(['insertWidget',{
+      // TODO: Replace this widget ID with one from Cxense.
+      widgetId: '69c0ca55586b5c0856d4286c623e3c66017b5ad5',
+      renderFunction: this._render
+    }]);
+  }
+
+  _render(data, context) {
+    console.log(`RESPONSE FROM CXENSE!`);
+    console.dir(context);
+    if (typeof data.response !== 'undefined' && data.response.items.length > 0) {
+      console.dir(data.response.items);
+    } 
+  }
 }
 Polymer(cxenseRecommendationWidget);
